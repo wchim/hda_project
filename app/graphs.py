4ee4f2d86e46e0e8fbb0a0bc4a68827304979b1a
wchim <wayne.chim01@gmail.com>
2022-12-29 21:03:35 -0500

    fixed time graph

--- app/graphs.py
+++ app/graphs.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 def weight_journey(user_df):
+        user_df.date = pd.to_datetime(user_df.date, infer_datetime_format=True)
         user = user_df.user.iloc[0]
         bodyweight_goal = user_df.bw_goal.mean()
         bodyweight_avg = user_df.wt_lb.mean()
@@ -15,8 +16,8 @@ def weight_journey(user_df):
                          'wt_lb': 'Bodyweight (lbs)',
                          'time_of_day': 'Time of Day'},
                          trendline='rolling',
-                         trendline_options=dict(window=1),
-                         trendline_color_override='yellow',
+                         trendline_options=dict(window=7),
+                         trendline_color_override='gray',
                          opacity=0.7,
                          color='time_of_day',
                          color_discrete_map={
